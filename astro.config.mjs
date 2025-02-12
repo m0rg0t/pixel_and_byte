cc571017a9739d6868ee0f4cb805d0b827f26f2a
Anton Lenev <m0rg0t.Anton@gmail.com>
2025-02-13 00:31:45 +0300

    Init

---
+++ astro.config.mjs
@@ -0,0 +1,36 @@
+// // @ts-check
+// import { defineConfig } from 'astro/config';
+
+// import react from '@astrojs/react';
+
+// // https://astro.build/config
+// export default defineConfig({
+//   integrations: [react()]
+// });
+
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind(), react()],
+  vite: {
+    resolve: {
+      alias: {
+        '@': '/src',
+        '@components': '/src/components'
+      }
+    }
+  },
+  output: 'static',
+  build: {
+    inlineStylesheets: 'auto'
+  },
+  server: {
+    host: true,
+    port: 4321,
+  }
+});
